f90ea908ac10f87513fbe577545eefd21f192cbc
aji prasetyo <ajiprxty3@gmail.com>
2025-03-07 11:39:16 +0700

    string and variable

---
+++ basic/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Aji")
+	fmt.Println("Aji Prasetyo")
+
+	fmt.Println(len("Aji"))
+	fmt.Println("Aji Prasetyo"[5])
+}
---
+++ basic/variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Aji"
+	fmt.Println(name)
+
+	name = "Aji Prasetyo"
+	fmt.Println(name)
+
+	var number = 38
+	fmt.Println(number)
+
+	var (
+		firstName  = "Aji"
+		lastName   = "Prasetyo"
+		numberInit = 37
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(numberInit)
+}

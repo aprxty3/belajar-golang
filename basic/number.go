db4244e869e65f18fb690542e9224956a65c7ecf
aji prasetyo <ajiprxty3@gmail.com>
2025-03-07 11:25:03 +0700

    number and bool

---
+++ basic/number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	x = 1
+	y := 2
+	x = x + y
+	println(x)
+
+	fmt.Println("Satu", 1)
+	fmt.Println("Dua", 2)
+	fmt.Println("Tiga", 3)
+	fmt.Println("Empat koma lima", 4.5)
+}
